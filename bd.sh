e79913ac6b1a1105e939a22e2dfa7d244b4be3c6
M Mahrous <m.mahrous@thed.io>
2016-12-10 08:54:40 +0200

    init

---
+++ bd.sh
@@ -0,0 +1,14 @@
+################################################
+########## Bootstrap deployment 2016 H4C #######
+################################################
+
+# Install docker 
+
+curl -fsSL https://get.docker.com/ | sh
+
+service docker start
+
+# Installing docker compose 
+curl -L "https://github.com/docker/compose/releases/download/1.7.0/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+
+chmod a+x /usr/local/bin/docker-compose
