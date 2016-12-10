5882df09e63dbb6ca81d0d7fa0290cfbdc4849b0
M Mahrous <m.mahrous@thed.io>
2016-12-10 09:19:40 +0200

    update header

--- bd.sh
+++ bd.sh
@@ -8,7 +8,11 @@ curl -fsSL https://get.docker.com/ | sh
 
 service docker start
 
+echo "Docker installed :D"
 # Installing docker compose 
 curl -L "https://github.com/docker/compose/releases/download/1.7.0/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
 
 chmod a+x /usr/local/bin/docker-compose
+
+echo "Compose installed =D"
+
